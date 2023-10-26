4f309afce77fe5e1d9b082ea4eafe703683874b7
Glenn Molina <glenn.gleber.molinaofc@outlook.com>
2023-10-25 21:42:42 -0300

    Primeiro commit

---
+++ Projeto 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num1;
+            decimal num2;
+            decimal num3;
+            decimal num4;
+
+            decimal media;
+            decimal numeros = 4;
+            decimal som_num;
+
+            Console.WriteLine("informe o primeiro número:");
+            num1 = Convert.ToDecimal(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("informe o primeiro número:");
+            num2 = Convert.ToDecimal(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("informe o primeiro número:");
+            num3 = Convert.ToDecimal(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("informe o primeiro número:");
+            num4 = Convert.ToDecimal(Console.ReadLine());
+            Console.Clear();
+
+            som_num = num1 + num2 + num3 + num4;
+            media = som_num / numeros;
+
+            Console.WriteLine("A média dos 4 números é:"+ media);
+
+
+
+
+
+        }
+    }
+}
---
+++ Projeto 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            decimal idade;
+            char sexo;
+            decimal peso;
+            decimal altura;
+            decimal imc;
+
+            Console.WriteLine("Qual seu nome?");
+            Console.ReadLine();
+
+            Console.WriteLine("Quantos anos vc tem?");
+            idade = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine(
+
+                "infome M para o sexo Masxulino e F para o sexo Feminino"
+
+                );
+            sexo = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Qual seu peso?");
+            peso = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Qual sua altura?");
+            altura = Convert.ToDecimal(Console.ReadLine());
+
+            imc = altura * altura / peso;
+            Console.WriteLine("Seu IMC é de:" + imc );
+        }
+    }
+}
